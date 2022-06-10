a943a696efba7b49dd01f52b0eb8c078e81acfa2
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-06-10 19:57:23 -0300

    fix: linters

---
+++ src/scripts/clone-gitops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -z "$GITOPS_REPOSITORY" ]]; then
+  echo "Missing GITOPS_REPOSITORY environment"
+  exit 1
+fi
+if [[ -z "$GITHUB_ACCESS_TOKEN" ]]; then
+  echo "Missing GITHUB_ACCESS_TOKEN environment"
+  exit 1
+fi
+
+git clone https://${GITHUB_ACCESS_TOKEN}@github.com/dafiti-group/${GITOPS_REPOSITORY}.git
