ea5ce99b084f66d7075a6b73101569d908d2235b
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-06-10 19:36:36 -0300

    fix: :zap: :ninja:

--- src/scripts/sonar-scanner.sh
+++ src/scripts/sonar-scanner.sh
@@ -6,5 +6,5 @@ sonar-scanner \
   -Dsonar.projectBaseDir=${PWD} \
   -Dsonar.projectKey=${CIRCLE_PROJECT_REPONAME} \
   -Dsonar.projectName=${CIRCLE_PROJECT_REPONAME} \
-  -Dsonar.scm.revision="$(echo ${CIRCLE_SHA1:0:7})" \
+  -Dsonar.scm.revision="${CIRCLE_SHA1:0:7}" \
   -Dsonar.sourceEncoding=UTF-8
