100ca966aa1d9b3a55ae9609eb52aeb59f9c68d0
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-06-10 21:22:51 -0300

    :tractor:

---
+++ src/scripts/hold-slack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 'export PARAMETER_STEP_NAME="${PARAMETER_TO_EXPORT}"' >> $BASH_ENV
--- src/scripts/on-hold.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo 'export PARAMETER_STEP_NAME="<<parameters.step_name>>"' >> $BASH_ENV
