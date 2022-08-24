9df839f0c03a8ef94e8f8223cc27f43206f8f9a1
Gritzko Daniel Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-08-24 15:27:06 -0300

    feat: crate new inputs to eks-deploy and eks-promote (#6)
    * feat: crate new inputs to eks-deploy and eks-promote
    
    - `job/eks-deploy` included `file_name` to arbitraty change the name of the file that will be change the image tag from. Used to new archtectury using the new repository `gitops` for deployments.
    - `job/eks-promote` included `origin_file` and `destiny_file` to arbitraty change the name of the file that will be change the image tag from. Used to new archtectury using the new repository `gitops` for deployments.
    
    * fix: Update extend file.
    
    Co-authored-by: Cayke Manjanelli <cayke@hotmail.com>

--- src/scripts/gitops-promote.sh
+++ src/scripts/gitops-promote.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [[ -z "$PARAMETERS_ORIGIN_FILE" ]]; then
-  echo "Missing PARAMETERS_ORIGIN_FILE environment"
-  exit 1
-fi
-if [[ -z "$PARAMETERS_DESTINY_FILE" ]]; then
-  echo "Missing PARAMETERS_DESTINY_FILE environment"
+if [[ -z "$PARAMETERS_ORIGIN_PATH" ]]; then
+  echo "Missing PARAMETERS_ORIGIN_PATH environment"
   exit 1
 fi
 
-ORIGIN_FILE="${PARAMETERS_ORIGIN_FILE}/${CIRCLE_PROJECT_REPONAME}.yaml"
+if [[ -z "$PARAMETERS_DESTINY_PATH" ]]; then
+  echo "Missing PARAMETERS_DESTINY_PATH environment"
+  exit 1
+fi
+
+ORIGIN_FILE="${PARAMETERS_ORIGIN_PATH}/${PARAMETERS_ORIGIN_FILE:-$CIRCLE_PROJECT_REPONAME}.yaml"
 if [ ! -f "${ORIGIN_FILE}" ]; then
   echo "file ${ORIGIN_FILE} not found"
   exit 1
 fi
-DESTINY_FILE="${PARAMETERS_DESTINY_FILE}/${CIRCLE_PROJECT_REPONAME}.yaml"
+DESTINY_FILE="${PARAMETERS_DESTINY_PATH}/${PARAMETERS_DESTINY_FILE:-$CIRCLE_PROJECT_REPONAME}.yaml"
 if [ ! -f "${DESTINY_FILE}" ]; then
   echo "file ${DESTINY_FILE} not found"
   exit 1
@@ -25,7 +26,7 @@ cd ${PARAMETER_START_FOLDER} || exit 1
 if [[ $(git diff) ]]; then
   git diff
   git add .
-  git commit -m "${CIRCLE_PROJECT_REPONAME} promoting image from  ${PARAMETERS_ORIGIN_FILE} to ${PARAMETERS_DESTINY_FILE}"
+  git commit -m "${CIRCLE_PROJECT_REPONAME} promoting image from ${ORIGIN_FILE} to ${DESTINY_FILE}"
   git push
 else
   echo "Nothing to commit, promotion already done!"
