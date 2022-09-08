ad2dda722dabc46d186f63f81bd583451761fb8f
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-09-08 20:38:26 -0300

    fix: bash lint SC2089

--- src/scripts/grafana-notify.sh
+++ src/scripts/grafana-notify.sh
@@ -13,13 +13,13 @@ export TZ="America/Sao_Paulo"
 LOCAL_GITHUB_URL="https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/commit/${CIRCLE_SHA1}"
 TIMESTAMP=$(date +%s)
 TIMENOW=$(date -R)
-MESSAGE="{ \"what\": \"App:${CIRCLE_PROJECT_REPONAME}\", \"tags\": [ \"pipeline-notifications-${PARAMETER_ENV}\" ], \"when\": ${TIMESTAMP}, \"data\": \"Time: ${TIMENOW} Circleci link: ${CIRCLE_BUILD_URL} commit: ${LOCAL_GITHUB_URL}\" }"
+MESSAGE=$(echo "{ \"what\": \"App:${CIRCLE_PROJECT_REPONAME}\", \"tags\": [ \"pipeline-notifications-${PARAMETER_ENV}\" ], \"when\": ${TIMESTAMP}, \"data\": \"Time: ${TIMENOW} Circleci link: ${CIRCLE_BUILD_URL} commit: ${LOCAL_GITHUB_URL}\" }")
 GRAFANA_API="${GRAFANA_URL}/api/annotations/graphite"
 AUTH_TOKEN="Authorization: Bearer ${GRAFANA_TOKEN}"
 REQ_TYPE='Content-Type: application/json'
 echo "validation message"
 echo ""
-echo $MESSAGE | jq
+echo "${MESSAGE}" | jq
 RESPONSE=$(curl --location --request POST "${GRAFANA_API}" -H "${AUTH_TOKEN}" -H "${REQ_TYPE}" --data "$MESSAGE")
 RESPONSE_MESSAGE=$(echo ${RESPONSE} | jq -r '.message')
 if [ "${RESPONSE_MESSAGE}" != "Graphite annotation added" ]; then
