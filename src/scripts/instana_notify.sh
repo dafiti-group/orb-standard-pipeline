faef4349f98476fe8041317a383782001b354ba9
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-10-20 18:43:42 -0300

    chore: incorporate instana notify command

--- src/scripts/instana_notify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BODY_MODIFIED=$(echo "${PARAMETER_TEMPLATE}" | jq '.' | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed 's/`/\\`/g')
-CUSTOM_BODY_MODIFIED=$(eval echo \""${BODY_MODIFIED}"\" )
-echo $CUSTOM_BODY_MODIFIED
-exit 1
