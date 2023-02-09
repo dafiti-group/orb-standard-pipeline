5c8c7587db75623499f059348022c0558aec977a
Cayke Xavier <cayke.xavier@dafiti.com.br>
2023-02-09 18:03:33 -0300

    feat: include instana new version

--- src/scripts/create_release.sh
+++ src/scripts/create_release.sh
@@ -1,63 +1,27 @@
 #!/bin/bash
 
-set -x
-set -eo pipefail
+echo "Validating envs!"
+if [[ -z "$LOCAL_SERVICE_NAME" ]]; then
+  echo "Missing LOCAL_SERVICE_NAME environment used in pipeline-feedback/create_release command"
+  exit 1
+fi
+if [[ -z "$LOCAL_RELEASE_SCOPE" ]]; then
+  echo "Missing LOCAL_RELEASE_SCOPE environment used in pipeline-feedback/create_release command"
+  exit 1
+fi
 
-readonly version='dev'
+INTERNAL_JSON_STRING_FULL=$(echo $LOCAL_RELEASE_SCOPE | jq -c | jq -R)
+INTERNAL_INTERPOLATED_JSON=$(echo $INTERNAL_JSON_STRING_FULL | envsubst)
 
-function create_release() {
-    missing_dependencies=false
+echo "${INTERNAL_INTERPOLATED_JSON}" >internal_scope.json
+echo "Validation JSON"
+if ! INTERNAL_OUTPUT=$(jq empty internal_scope.json 2>&1); then
+  echo "Scope JSON is valid: ${INTERNAL_OUTPUT}"
+  exit 1
+fi
 
-    if ! which curl; then
-        echo 'Missing dependency: The curl command is not available'
-        missing_dependencies=true
-    fi
-
-    if ! which jq; then
-        echo 'Missing dependency: The jq command is not available'
-        missing_dependencies=true
-    fi
-
-    if [ ${missing_dependencies} == true ]; then
-        echo 'Missing dependencies detected, aborting'
-        exit 1
-    fi
-
-    if which envsubst; then
-        release_name=$(echo "${INSTANA_RELEASE_NAME}" | envsubst)
-    else
-        echo 'The envsubst command is not available, skipping the interpolation of environment variables in the release name'
-        release_name="${INSTANA_RELEASE_NAME}"
-    fi
-
-    echo "Creating release '${release_name}'"
-
-    if [ -z "${INSTANA_RELEASE_SCOPE}" ]; then
-        INSTANA_RELEASE_SCOPE='{}'
-    fi
-
-    JSON_STRING_FULL=$(echo $INSTANA_RELEASE_SCOPE | jq -c | jq -R)
-    INTERPOLATED_JSON=$(eval echo $JSON_STRING_FULL)
-    echo "${INTERPOLATED_JSON}" > scope.json
-
-    if ! OUTPUT=$(jq empty scope.json 2>&1); then
-        echo "Scope JSON is valid: ${OUTPUT}"
-        exit 1
-    fi
-
-    curl --location --request POST "${!INSTANA_ENDPOINT_URL_NAME}/api/releases" \
-        --silent \
-        --fail \
-        --show-error \
-        --header "Authorization: apiToken ${!INSTANA_API_TOKEN_NAME}" \
-        --header 'Content-Type: application/json' \
-        --user-agent "instana/pipeline-feedback-orb/${version}" \
-        --data "{
-    \"name\": \"${release_name}\",
-    \"start\": $(date +%s)000,
-    \"applications\": $(jq -r '.applications' < scope.json),
-    \"services\": $(jq -r '.services' < scope.json)
-}" | jq -r ".id" | xargs -I {} echo "New release created with id {}"
-}
-
-create_release
+echo "Exporting context env to next steps"
+# EXPORTING ENVS TO BE USED IN THE pipeline-feedback/create_release command ============================================
+{
+  echo "export INTERNAL_INTERPOLATED_JSON=${INTERNAL_INTERPOLATED_JSON}"
+} >>"${BASH_ENV}"
