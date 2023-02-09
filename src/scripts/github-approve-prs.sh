01341772af2440cafa735fce7c4737cadf3dcb9d
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2023-02-09 11:52:02 -0300

    fix: SC2046

--- src/scripts/github-approve-prs.sh
+++ src/scripts/github-approve-prs.sh
@@ -5,12 +5,12 @@ LOCAL_PARSED_BOT_NAME=$(eval echo $LOCAL_DFT_BOT_NAME)
 if [[ $(gh pr list -B ${LOCAL_HEAD_GIT_BRANCH}) ]]; then
   echo "PR open listed. Notify to update"
   gh pr list -B ${LOCAL_HEAD_GIT_BRANCH} | awk '{print$1}' | while read -r line; do
-    read -r LOCAL_AUTHOR LOCAL_BRANCH_ORIGIN <<<$(gh pr view $line --json 'headRefName,author' --jq '[.author.login,.headRefName] | join(" ")')
+    read -r LOCAL_AUTHOR LOCAL_BRANCH_ORIGIN <<<"$(gh pr view $line --json 'headRefName,author' --jq '[.author.login,.headRefName] | join(" ")')"
     LOCAL_STRING="PR $line of author: ${LOCAL_AUTHOR} and origin: ${LOCAL_BRANCH_ORIGIN} target;${LOCAL_HEAD_GIT_BRANCH}"
     if [[ "${LOCAL_AUTHOR}" != "${LOCAL_PARSED_BOT_NAME}" ]]; then
       if [[ "${LOCAL_BRANCH_ORIGIN}" != "${CIRCLE_BRANCH}" ]]; then
         LOCAL_ACTION="blocked"
-        gh pr review $line --request-changes --body ":robot: ${LOCAL_PARSED_BOT_NAME} blocked this PR because other pipeline changed the $(image.tag) hash of this artifact in the test environment. \
+        gh pr review $line --request-changes --body ":robot: ${LOCAL_PARSED_BOT_NAME} blocked this PR because other pipeline changed the \`image.tag\` hash of this artifact in the test environment. \
           You need to re-run the pipeline in order to set the hash of your artifact in the test environment to continue with this PR."
       else
         LOCAL_ACTION="approved"
