c7fcb5a2b3f667b1caefa6a08ecdb6796cd114dd
Gritzko Daniel Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2023-02-10 10:33:32 -0300

    [PLT-523] - fix pipeline agent approve image tag sed pattern and instana notification (#18)
    * [PLT-523] - fix pattern eks-promote sed
    
    * update docs
    
    * chore: fix github approve job
    
    * chore: change `envsubst` to `eval` in github-approve
    
    * fix: SC2046
    
    * chore: update PR author name
    
    * feat: include instana new version
    
    * update description instana notify
    
    ---------
    
    Co-authored-by: Cayke Xavier <cayke.xavier@dafiti.com.br>

--- src/scripts/github-approve-prs.sh
+++ src/scripts/github-approve-prs.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-if [[ $(gh pr list) ]]; then
+
+LOCAL_HEAD_GIT_BRANCH=$(git branch --list --remote | grep "origin/HEAD ->" | awk -F"/" '{print$3}')
+LOCAL_PARSED_BOT_NAME=$(eval echo $LOCAL_DFT_BOT_NAME)
+if [[ $(gh pr list -B ${LOCAL_HEAD_GIT_BRANCH}) ]]; then
   echo "PR open listed. Notify to update"
-  gh pr list | awk '{print$1}' | while read -r line; do
-    if [ "$(gh pr view $line --json author --jq '.author.login')" != "dft-deploy" ]; then
-      gh pr review $line --request-changes --body "The staging env was chaged! You need to re-run this pipeline to create a new deploy to staging"
-    fi
-  done
-fi
-if [[ $(gh pr list -H ${CIRCLE_BRANCH}) ]]; then
-  echo "PR open listed. APPROVE"
-  gh pr list -H ${CIRCLE_BRANCH} | awk '{print$1}' | while read -r line; do
-    if [ "$(gh pr view $line --json author --jq '.author.login')" != "dft-deploy" ]; then
-      gh pr review $line --approve --body "Thi PR is ready to merge!"
+  gh pr list -B ${LOCAL_HEAD_GIT_BRANCH} | awk '{print$1}' | while read -r line; do
+    read -r LOCAL_PR_AUTHOR LOCAL_BRANCH_ORIGIN <<<"$(gh pr view $line --json 'headRefName,author' --jq '[.author.login,.headRefName] | join(" ")')"
+    LOCAL_STRING="PR $line of author: ${LOCAL_PR_AUTHOR} and origin: ${LOCAL_BRANCH_ORIGIN} target;${LOCAL_HEAD_GIT_BRANCH}"
+    if [[ "${LOCAL_PR_AUTHOR}" != "${LOCAL_PARSED_BOT_NAME}" ]]; then
+      if [[ "${LOCAL_BRANCH_ORIGIN}" != "${CIRCLE_BRANCH}" ]]; then
+        LOCAL_ACTION="blocked"
+        gh pr review $line --request-changes --body ":robot: ${LOCAL_PARSED_BOT_NAME} blocked this PR because other pipeline changed the \`image.tag\` hash of this artifact in the test environment. \
+          You need to re-run the pipeline in order to set the hash of your artifact in the test environment to continue with this PR."
+      else
+        LOCAL_ACTION="approved"
+        gh pr review $line --approve --body ":robot: This PR is ready to merge! :rocket:"
+      fi
     else
+      LOCAL_ACTION="rejected"
       gh pr close $line
-      git push -d origin ${CIRCLE_BRANCH}
+      git push -d origin ${LOCAL_BRANCH_ORIGIN}
     fi
+    echo "${LOCAL_STRING} ${LOCAL_ACTION}"
   done
-else
-  echo "Noting to do, No PR found, done!"
 fi
