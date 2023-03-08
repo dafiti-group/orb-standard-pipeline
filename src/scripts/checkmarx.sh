fc3984edecccfefaca1ba7aeb1b7e6343053cdf9
Cayke Xavier <cayke.xavier@dafiti.com.br>
2023-03-08 10:01:21 -0300

    update checkmarx with logic "delete last scan ID"

--- src/scripts/checkmarx.sh
+++ src/scripts/checkmarx.sh
@@ -12,10 +12,6 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
   echo "Missing GITHUB_TOKEN environment used in cxflow/scan command"
   exit 1
 fi
-if [[ -z "$HEAD_BRANCH_NAME" ]]; then
-  echo "Missing HEAD_BRANCH_NAME environment"
-  exit 1
-fi
 if [[ -z "$CHECKMARX_URL" ]]; then
   echo "Missing CHECKMARX_URL environment"
   exit 1
@@ -46,12 +42,49 @@ echo "Set up the initials envs of the context"
 PARAMETER_BRANCH_NAME_SANITIZED=$(echo ${CIRCLE_BRANCH} | sed 's|\/|-|g' | sed 's|_|-|g' | tr '[:upper:]' '[:lower:]')
 PARAMETER_PROJECT_BRANCH_NAME="${CIRCLE_PROJECT_REPONAME}.${PARAMETER_BRANCH_NAME_SANITIZED}"
 # CREATING THE ENV TO HANDLE SYMBOLIC LINKS IN THE NEXT STEP
+PARAMETER_ADITIONAL_CHECKMARX_ARGS=""
+THRESHOLDS_HIGH_CHECKMARX=""
+THRESHOLDS_MEDIUM_CHECKMARX=""
+
+#Init local envs
+PROJECT_LIST=""
+PROJECT=""
+PROJECT_ID=""
+BRANCH=""
+
 PARAMETER_SYMBOLIC_FILES=""
 if [[ $(find . -type l) ]]; then
   PARAMETER_SYMBOLIC_FILES=$(find . -type l | cut -c 3- | paste -sd ",")
 fi
 
 # =================================================================================================
+
+# Checkmarx API - Auth method
+function auth_identity() {
+  echo "Auth method to get the access token..."
+  AUTH_RESPONSE=$(
+    curl \
+      --location \
+      --request POST "${CHECKMARX_URL}/cxrestapi/auth/identity/connect/token" \
+      --silent \
+      --show-error \
+      --header 'Content-Type: application/x-www-form-urlencoded' \
+      --data-urlencode "username=${CHECKMARX_USERNAME}" \
+      --data-urlencode "password=${CHECKMARX_PASSWORD}" \
+      --data-urlencode 'grant_type=password' \
+      --data-urlencode 'scope=access_control_api sast_api' \
+      --data-urlencode 'client_id=resource_owner_sast_client' \
+      --data-urlencode "client_secret=${CHECKMARX_CLIENT_SECRET}"
+  )
+
+  BEARER_TOKEN=$(echo ${AUTH_RESPONSE} | jq -r '.access_token')
+
+  if [[ "${BEARER_TOKEN}" == null ]]; then
+    echo "Auth failed: ${RESULT}"
+    exit 1
+  fi
+  echo "Auth success!"
+}
 
 # Checkmarx API - create_branch method
 function create_branch() {
@@ -62,7 +95,6 @@ function create_branch() {
       --location \
       --request POST "${CHECKMARX_URL}/cxrestapi/projects/${PROJECT_ID}/branch" \
       --silent \
-      --fail-with-body \
       --show-error \
       --header 'Content-Type: application/json' \
       --header "Authorization: Bearer ${BEARER_TOKEN}" \
@@ -83,7 +115,6 @@ function delete_branch() {
       --location \
       --request DELETE "${CHECKMARX_URL}/cxrestapi/help/projects/${*}" \
       --silent \
-      --fail-with-body \
       --show-error \
       --header 'Content-Type: application/json;v=1.0' \
       --header "Authorization: Bearer ${BEARER_TOKEN}" \
@@ -93,6 +124,12 @@ function delete_branch() {
 }
 # Local helper function to compare branch list to be excluded from Checkmarx
 function search_branchs_to_delete_in_checkmarx() {
+  echo "Validating HEAD_BRANCH_NAME env!"
+  if [[ -z "$HEAD_BRANCH_NAME" ]]; then
+    echo "Missing HEAD_BRANCH_NAME environment"
+    exit 1
+  fi
+
   echo "Searching for branchs to delete in checkmarx"
   git config --global --add safe.directory "*"
   ALL_BRANCHS=$(git branch --list --remotes | sed -E 's|^\s+||g')
@@ -100,15 +137,15 @@ function search_branchs_to_delete_in_checkmarx() {
   CHECKMARX_LOCAL_PROJECTS=$(echo "${PROJECT_LIST}" | grep -Eo "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}\..*" | grep -v ${HEAD_BRANCH_NAME})
   if [[ -n "${BRANCH_LIST}" && -n "${CHECKMARX_LOCAL_PROJECTS}" ]]; then
     echo "
-=============================================
-github branch names sanitized:
-${BRANCH_LIST}
-=============================================
-checkmarx projects
-${CHECKMARX_LOCAL_PROJECTS}
-=============================================
-LOOPING TO FIND BRANCHS TO DELETE IN CHECKMARX....
-"
+  =============================================
+  github branch names sanitized:
+  ${BRANCH_LIST}
+  =============================================
+  checkmarx projects
+  ${CHECKMARX_LOCAL_PROJECTS}
+  =============================================
+  LOOPING TO FIND BRANCHS TO DELETE IN CHECKMARX....
+  "
     echo "${CHECKMARX_LOCAL_PROJECTS}" | while read -r line; do
       TMP=$(echo $line | rev | cut -d\. -f1 | rev)
       ID=$(echo "$line" | awk '{print$1}')
@@ -125,70 +162,118 @@ LOOPING TO FIND BRANCHS TO DELETE IN CHECKMARX....
   fi
 
 }
-
-echo "Auth method to get the access token..."
-# Checkmarx API - Auth method
-AUTH_RESPONSE=$(
-  curl \
-    --location \
-    --request POST "${CHECKMARX_URL}/cxrestapi/auth/identity/connect/token" \
-    --silent \
-    --fail-with-body \
-    --show-error \
-    --header 'Content-Type: application/x-www-form-urlencoded' \
-    --data-urlencode "username=${CHECKMARX_USERNAME}" \
-    --data-urlencode "password=${CHECKMARX_PASSWORD}" \
-    --data-urlencode 'grant_type=password' \
-    --data-urlencode 'scope=access_control_api sast_api' \
-    --data-urlencode 'client_id=resource_owner_sast_client' \
-    --data-urlencode "client_secret=${CHECKMARX_CLIENT_SECRET}"
-)
-
-BEARER_TOKEN=$(echo ${AUTH_RESPONSE} | jq -r '.access_token')
-
-if [[ "${BEARER_TOKEN}" == null ]]; then
-  echo "Auth failed: ${RESULT}"
-  exit 1
-fi
-echo "Auth success, listing projects..."
 # Checkmarx API - project_exists method
-PROJECT_LIST_RESPONSE=$(
-  curl \
-    --location \
-    --request GET "${CHECKMARX_URL}/cxrestapi/projects" \
-    --silent \
-    --fail-with-body \
-    --show-error \
-    --header "Authorization: Bearer ${BEARER_TOKEN}"
-)
+function get_projects() {
+  echo "Get project list ..."
+  PROJECT_LIST_RESPONSE=$(
+    curl \
+      --location \
+      --request GET "${CHECKMARX_URL}/cxrestapi/projects" \
+      --silent \
+      --show-error \
+      --header "Authorization: Bearer ${BEARER_TOKEN}"
+  )
 
-echo "Projects list request executed with success, Searching for project: ${CIRCLE_PROJECT_REPONAME}"
+  echo "Projects list request executed with success, Searching for project: ${CIRCLE_PROJECT_REPONAME}"
+  echo "with Branch: ${PARAMETER_PROJECT_BRANCH_NAME}"
 
-PROJECT_LIST=$(echo ${PROJECT_LIST_RESPONSE} | jq -r '.[] as $response | [$response.id,$response.name] | join(" ")')
-PARAMETER_ADITIONAL_CHECKMARX_ARGS=""
-if echo "${PROJECT_LIST}" | grep -Eq "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}$"; then
-  PARAMETER_ADITIONAL_CHECKMARX_ARGS="0"
-  PROJECT=$(echo "${PROJECT_LIST}" | grep -Eo "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}$")
-  PROJECT_ID=$(echo ${PROJECT} | awk '{print$1}')
-  echo "Project Found: ${PROJECT} . Verifing if branch: ${PARAMETER_BRANCH_NAME_SANITIZED} exists."
+  PROJECT_LIST=$(echo ${PROJECT_LIST_RESPONSE} | jq -r '.[] as $response | [$response.id,$response.name] | join(" ")')
 
+  if echo "${PROJECT_LIST}" | grep -Eq "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}$"; then
+    PROJECT=$(echo "${PROJECT_LIST}" | grep -Eo "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}$")
+    PROJECT_ID=$(echo ${PROJECT} | awk '{print$1}')
+  fi
   if echo "${PROJECT_LIST}" | grep -Eq "^[0-9]+ ${PARAMETER_PROJECT_BRANCH_NAME}$"; then
     BRANCH=$(echo "${PROJECT_LIST}" | grep -Eo "^[0-9]+ ${PARAMETER_PROJECT_BRANCH_NAME}$")
-    echo "Branch already exists: ${BRANCH}"
+    PROJECT_ID=$(echo ${BRANCH} | awk '{print$1}')
+  fi
+}
+
+# Checkmarx API - project_exists method
+function project_exists() {
+  # Get project list
+  get_projects
+
+  echo "Project Type: " ${PROJECT_TYPE}
+  if [[ ${PROJECT} != "" ]]; then
+    # Projeto Legado ou Novo + Existente
+    THRESHOLDS_HIGH_CHECKMARX="0"
+    THRESHOLDS_MEDIUM_CHECKMARX="0"
+    echo "Project Found: ${PROJECT} . Verifing if branch: ${PARAMETER_BRANCH_NAME_SANITIZED} exists."
+
+    if [[ ${BRANCH} == "" ]]; then
+      echo "Branch not found, trying to create one!"
+      create_branch
+    else
+      echo "Branch already exists: ${BRANCH}"
+    fi
+    if echo "${PROJECT_LIST}" | grep -Eq "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}\..*$"; then
+      search_branchs_to_delete_in_checkmarx
+    fi
   else
-    echo "Branch not found, trying to create one!"
-    create_branch
+    PARAMETER_PROJECT_BRANCH_NAME="${CIRCLE_PROJECT_REPONAME}"
+    if [[ ${PROJECT_TYPE} != "legacy" ]]; then
+      echo "Set Thresholds"
+      THRESHOLDS_HIGH_CHECKMARX="0"
+      THRESHOLDS_MEDIUM_CHECKMARX="0"
+    fi
+    echo "Project not found, ready to execute next step. The project listed is: "
+    echo "${PROJECT_LIST}"
   fi
-  if echo "${PROJECT_LIST}" | grep -Eq "^[0-9]+ ${CIRCLE_PROJECT_REPONAME}\..*$"; then
-    search_branchs_to_delete_in_checkmarx
+}
+# Checkmarx API - Get Last Scan ID
+function get_last_scan_id() {
+  echo "deleting last scan ID - Sleep 5s"
+  sleep 5
+  # Get project list
+  get_projects
+  echo "Get last scan ID for Project: ${PROJECT_ID}"
+  LAST_SCAN_ID=""
+  SCAN_LIST_RESPONSE=$(
+    curl \
+      --location \
+      --request GET "${CHECKMARX_URL}/Cxwebinterface/odata/v1/Projects/${PROJECT_ID}" \
+      --silent \
+      --show-error \
+      --header "Authorization: Bearer ${BEARER_TOKEN}"
+  )
+  echo "Request last scan ID executed with success, validating response."
+  if [[ $(echo ${SCAN_LIST_RESPONSE} | jq '.value' | jq '.[0].LastScanId') == null ]]; then
+    echo "Last scan ID not found: ${SCAN_LIST_RESPONSE}"
+    exit 1
   fi
-else
-  PARAMETER_PROJECT_BRANCH_NAME="${CIRCLE_PROJECT_REPONAME}"
-  echo "Project not found, ready to execute next step. The project listed is: "
-  echo "${PROJECT_LIST}"
-fi
-echo "PARAMETER_ADITIONAL_CHECKMARX_ARGS: "
-echo ${PARAMETER_ADITIONAL_CHECKMARX_ARGS}
+  LAST_SCAN_ID=$(echo ${SCAN_LIST_RESPONSE} | jq '.value' | jq '.[0].LastScanId')
+  echo "Last scan ID identified! ${LAST_SCAN_ID}"
+
+  echo "Calling delete last scan ID method!"
+  delete_last_scan_id
+
+}
+# Checkmarx API - delete_last scan ID method
+function delete_last_scan_id() {
+
+  echo "deleting last scan ID project ID: ${PROJECT_ID}"
+  DELETE_LAST_SCAN_ID_RESPONSE=$(
+    curl \
+      --location \
+      --request DELETE "${CHECKMARX_URL}/cxrestapi/sast/scans/${LAST_SCAN_ID}" \
+      --silent \
+      --show-error \
+      --header 'Content-Type: application/json;v=1.0' \
+      --header "Authorization: Bearer ${BEARER_TOKEN}" \
+      --data-raw "{\"deleteRunningScans\": \"true\"}"
+  )
+  echo "Last scan ID deleted with success! ${DELETE_LAST_SCAN_ID_RESPONSE}"
+}
+
+###### Main ######
+
+# Call autentication function
+auth_identity
+
+# Function that execute
+$PARAMETER_METHODE
+
 echo "Exporting context env to next steps"
 # EXPORTING ENVS TO BE USED IN THE cxflow/scan command ============================================
 {
@@ -196,6 +281,9 @@ echo "Exporting context env to next steps"
   echo "export PARAMETER_BRANCH_NAME_SANITIZED=${PARAMETER_BRANCH_NAME_SANITIZED}"
   echo "export PARAMETER_PROJECT_BRANCH_NAME=${PARAMETER_PROJECT_BRANCH_NAME}"
   echo "export PARAMETER_ADITIONAL_CHECKMARX_ARGS=${PARAMETER_ADITIONAL_CHECKMARX_ARGS}"
+  echo "export THRESHOLDS_HIGH_CHECKMARX=${THRESHOLDS_HIGH_CHECKMARX}"
+  echo "export THRESHOLDS_MEDIUM_CHECKMARX=${THRESHOLDS_MEDIUM_CHECKMARX}"
+
 } >>"${BASH_ENV}"
 
 echo "Finished with success!!!"
