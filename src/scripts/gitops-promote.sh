05ccd6c13e6d57197814eef284821567b1035a18
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2023-03-23 12:01:14 -0300

    feat: update deployments

--- src/scripts/gitops-promote.sh
+++ src/scripts/gitops-promote.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-if [[ -z "$PARAMETERS_ORIGIN_PATH" ]]; then
-  echo "Missing PARAMETERS_ORIGIN_PATH environment"
+LOCAL_ORIGIN_PATH=$(eval echo "${PARAMETER_ORIGIN_PATH:-$PARAMETER_COMPOSITE_ORIGIN_PATH}")
+LOCAL_DESTINY_PATH=$(eval echo "${PARAMETER_DESTINY_PATH:-$PARAMETER_COMPOSITE_DESTINY_PATH}")
+
+if [[ ! -d "${LOCAL_ORIGIN_PATH}" ]]; then
+  echo "The path ${LOCAL_ORIGIN_PATH} does not exists"
+  exit 1
+fi
+if [[ ! -d "${LOCAL_DESTINY_PATH}" ]]; then
+  echo "The path ${LOCAL_DESTINY_PATH} does not exists"
   exit 1
 fi
 
-if [[ -z "$PARAMETERS_DESTINY_PATH" ]]; then
-  echo "Missing PARAMETERS_DESTINY_PATH environment"
-  exit 1
-fi
-
-ORIGIN_FILE="${PARAMETERS_ORIGIN_PATH}/${PARAMETERS_ORIGIN_FILE:-$CIRCLE_PROJECT_REPONAME}.yaml"
+ORIGIN_FILE=$(eval echo "${LOCAL_ORIGIN_PATH}/${PARAMETER_ORIGIN_FILE}.yaml")
 if [ ! -f "${ORIGIN_FILE}" ]; then
   echo "file ${ORIGIN_FILE} not found"
   exit 1
 fi
-DESTINY_FILE="${PARAMETERS_DESTINY_PATH}/${PARAMETERS_DESTINY_FILE:-$CIRCLE_PROJECT_REPONAME}.yaml"
+DESTINY_FILE=$(eval echo "${LOCAL_DESTINY_PATH}/${PARAMETER_DESTINY_FILE}.yaml")
 if [ ! -f "${DESTINY_FILE}" ]; then
   echo "file ${DESTINY_FILE} not found"
   exit 1
