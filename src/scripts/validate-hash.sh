bcb863e60b81243e5d8cc538f1bebf9433c23cee
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2023-07-24 12:25:21 -0300

    fix: validate hash to rollback in aws ecr

--- src/scripts/validate-hash.sh
+++ src/scripts/validate-hash.sh
@@ -4,8 +4,15 @@ if [[ -z "${PARAMETER_VERSION}" ]]; then
   echo "Missing parameter PARAMETER_VERSION"
   exit 1
 fi
-VALIDATE_VERSION=$(git rev-parse --verify ${PARAMETER_VERSION})
-if [[ -z "${VALIDATE_VERSION}" ]]; then
-  echo "Invalid hash version"
+
+echo "VALIDATING IF THE TAG:${PARAMETER_VERSION} EXISTIS IN AWS ECR"
+
+LOCAL_IMAGE_TAGS=$(aws ecr list-images --repository-name "${CIRCLE_PROJECT_REPONAME}" | jq -r '.imageIds[].imageTag')
+if echo $LOCAL_IMAGE_TAGS | grep -oq "${PARAMETER_VERSION}"; then
+  echo "Registry found, OK-2-GO"
+  exit 0
+else
+  echo "Registry not found. Available tags is:"
+  echo ${LOCAL_IMAGE_TAGS}
   exit 1
 fi
