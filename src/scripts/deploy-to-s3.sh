33447403d74a47bcf7395e2abfd456781593ce57
Gritzko Daniel Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2026-03-17 19:25:55 +0100

    fix: amazon q review fixes

--- src/scripts/deploy-to-s3.sh
+++ src/scripts/deploy-to-s3.sh
@@ -2,12 +2,12 @@
 
 if [[ -z "${PARAMETER_FOLDER}" ]]; then
   echo "the parameter ${PARAMETER_FOLDER} could not be empty"
-  exti 1
+  exit 1
 fi
 if [[ -z "${PARAMETER_BUCKET}" ]]; then
   echo "the parameter ${PARAMETER_BUCKET} could not be empty"
-  exti 1
+  exit 1
 fi
 
 RESOLVED_ARGUMENTS=$(eval echo "${PARAMETER_ARGUMENTS}")
-aws s3 sync ${PARAMETER_FOLDER} ${PARAMETER_BUCKET} ${RESOLVED_ARGUMENTS}
+aws s3 sync "${PARAMETER_FOLDER}" "${PARAMETER_BUCKET}" ${RESOLVED_ARGUMENTS}
