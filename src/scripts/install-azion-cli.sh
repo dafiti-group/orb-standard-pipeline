04773fc95948aefaf9b2bf7a8f2c60d462cc4fc9
Gritzko Daniel Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2026-03-20 00:13:20 +0100

    lint fix
    ```txt
    
    In ./src/scripts/install-azion-cli.sh line 8:
    cd /tmp
    ^-----^ SC2164 (warning): Use 'cd ... || exit' or 'cd ... || return' in case cd fails.
    ```

--- src/scripts/install-azion-cli.sh
+++ src/scripts/install-azion-cli.sh
@@ -5,7 +5,7 @@ if [ -z "${AZION_TOKEN}" ]; then
   exit 1
 fi
 
-cd /tmp
+cd /tmp || exit 1
 wget https://github.com/aziontech/azion/releases/download/4.18.0/azion_4.18.0_linux_amd64.zip
 unzip azion_4.18.0_linux_amd64.zip
 mv azion $HOME/bin
